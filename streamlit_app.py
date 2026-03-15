84d8bc949da4deea686b32debb778066fa65b5e1
Mokrane-dev <kechir.mokrane@gmail.com>
2026-03-15 17:17:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,5 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
         
- st.success('Your Smoothie is ordered!', icon="✅")
- st.success('Order Name:  '+ name_on_order, icon="✅")
+st.success('Your Smoothie is ordered!', icon="✅")
+st.success('Order Name:  '+ name_on_order, icon="✅")
