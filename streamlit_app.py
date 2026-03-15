1a593f4d1a7917a979625e7a85e68247a8ca635a
Mokrane-dev <kechir.mokrane@gmail.com>
2026-03-15 17:26:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -29,6 +30,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
         sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
@@ -46,10 +49,8 @@ if ingredients_list:
     #if ingredients_string:
         session.sql(my_insert_stmt).collect()
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
 
 #st.success('Your Smoothie is ordered!', icon="✅")
 #st.success('Order Name:  '+ name_on_order, icon="✅")
